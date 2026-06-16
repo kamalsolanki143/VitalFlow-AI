2e22be79a53e11bf678c940974cfd477428b2eb6
kamalsolanki143 <solankikamal55143@gmail.com>
2026-06-17 00:59:52 +0530

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://vital-flow-ai-azure.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
